b8c404137594c6c3260debc69868cf7131d09461
Matthew Callens <callensmatt@gmail.com>
2017-10-07 18:20:00 -0400

    bluemix config

---
+++ utils/bluemixConfig.js
@@ -0,0 +1,13 @@
+// bluemixConfig.js
+
+module.exports = {
+  speech: {
+    url: 'https://stream.watsonplatform.net/speech-to-text/api/v1/recognize',
+    username: '6bb5da6f-ad19-44c8-8f86-51a0620a1e39',
+    password: 'a60kHYJ20EYA',
+    headers: {
+      'Content-Type': 'audio/flac',
+      'Transfer-Encoding': 'chunked'
+    }
+  }
+}
