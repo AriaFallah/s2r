87d60d8573ea964ca3a4a71ed648ba50c6b52716
AriaFallah <aria@fallah.us>
2017-10-07 22:12:33 -0400

    ocd bullshit

--- elastic/init_elastic.py
+++ elastic/init_elastic.py
@@ -1,12 +1,10 @@
-################################################################################
-# Initialize elasticsearch to contain the McCormick API data
-################################################################################
 import json
 import requests
 
-BASE_URL    = 'http://localhost:9200/'
+BASE_URL = 'http://localhost:9200/'
 PRODUCTS_FN = '../data/products.json'
-RECIPES_FN  = '../data/recipes.json'
+RECIPES_FN = '../data/recipes.json'
+
 
 def put_into_elastic(fn):
     base_fn = fn.split('/')[-1].split('.')[0]
@@ -17,14 +15,17 @@ def put_into_elastic(fn):
     for key in spicy_keys:
         r = requests.put(
             BASE_URL + base_fn + '/id/' + key + '?pretty',
-            data = json.dumps(spicy_json[key]),
-            headers = {'Content-Type': 'application/json', 'Accept': 'application/json'}
+            data=json.dumps(spicy_json[key]),
+            headers={
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
+            }
         )
         if r.status_code != 200 and r.status_code != 201:
-            print r.status_code
+            print(r.status_code)
             return
-        
-    
+
+
 if __name__ == '__main__':
     put_into_elastic(PRODUCTS_FN)
     put_into_elastic(RECIPES_FN)
